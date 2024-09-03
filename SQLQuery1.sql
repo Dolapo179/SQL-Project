84362336036caeb8a9bc17a9606167e9b5a1fa36
Dolapo <dolapoadenitire@gmail.com>
2024-09-03 20:09:57 +0100

    remove comment

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,7 +2,7 @@ select [QUANTITY], countries, region
 from BREW_DATA;
 
 
---- session B7
+
 select Top 1
 brands,
 Sum(quantity) as Favourite_Brands_SouthSouth_Nigeria
@@ -12,7 +12,7 @@ and COUNTRIES = 'nigeria'
 group by brands
 order by Favourite_Brands_SouthSouth_Nigeria desc;
 
---- session B8
+
 select  brands,
 sum(quantity) as Budweiser_Consumption_Nigeria
 from BREW_DATA
@@ -20,7 +20,7 @@ where BRANDS = 'Budweiser'
 and COUNTRIES = 'Nigeria'
 group by brands;
 
---- session B9
+
 select brands,
 sum(quantity) as '2019_Budweiser_Consumption_Nigeria'
 from brew_data
